3c23eb3d538d62105ffe3b1aecedda6ec47debf2
jmezetin <jmezetin@gmail.com>
2015-06-06 12:33:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ define([
   'scripts/router' // Request router.js
 ], function($, _, Parse, Router){
   var initialize = function(){
-    //Put your parse keys here!
-    Parse.initialize("Application ID Key", "Javascript Key");
+    //Put your parse keys here! 
+    Parse.initialize("TlB2CnmQw3BEQ6g6Uop571mR5jCI57HwuePqIg9I",
+                   "1vD7kFLP9yKHv2dtQWRmapkR6vre06zRbsnKrLYY");
     // Pass in our Router module and call it's initialize function
     Router.initialize();
   }
